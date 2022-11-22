aa82f7da3666a49e9359501479998439de0de430
Peter Walsh <peterbug80228@gmail.com>
2022-11-22 00:30:25 -0700

    Clean up LED.h

--- LED.h
+++ LED.h
@@ -1,14 +1,18 @@
 #ifndef LED
 #define LED
 
-struct LED {
+typedef struct {
     int redVal, greenVal, blueVal, brightness;
-    int (*changeBrightness)(struct LED *, int newBrightness);
-    int (*changeColor)(struct LED *, int newRedVal, int newGreenVal, int newBlueVal);
-};
+} Led;
 
-extern const struct LEDClass {
-    struct LED (*new)(int redVal, int greenVal, int blueVal, int brightness);
-} LED;
+// Initialize the LED objects
+int init_LEDs(Led* led);
+
+// Change the brightness of the given LEDobject to a value given by targetBrightness (between 0-100) 
+int change_LED_Brightness(Led* led, int targetBrightness);
+
+// Change the color of the given LEDobject LED based on the RGB values given.
+// *RGB values could be uint8, as they range from 0-255*
+int change_LED_Color(Led* led, int newRedVal, int newGreenVal, int newBlueVal);
 
 #endif
