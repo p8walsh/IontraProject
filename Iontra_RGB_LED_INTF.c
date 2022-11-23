f2aad99e105c888d0d99fbaf177f8b772f83689c
Peter Walsh <peterbug80228@gmail.com>
2022-11-22 20:55:32 -0700

    Debugging

--- Iontra_RGB_LED_INTF.c
+++ Iontra_RGB_LED_INTF.c
@@ -32,6 +32,8 @@ int change_LED_Brightness(Led* led_pointer, int newBrightness){
     Led led = *led_pointer;
     
     led.brightness = newBrightness;
+
+    printf("\nLED brightness: %d", led.brightness);
     
     return 0;
 }
@@ -84,19 +86,19 @@ int main(void){
     // Use code to test what is being visually confirmed above
     for ( int i=0; i<length; i++ ){
         if ( ledArray[i].redVal != 255 ){
-            printf("Error: redVal not initialized correctly. redVal: %d: ", ledArray[i].redVal);
+            printf("\nError: redVal not initialized correctly. redVal: %d: ", ledArray[i].redVal);
             return 1;
         }
         if ( ledArray[i].greenVal != 255 ){
-            printf("Error: greenVal not initialized correctly. greenVal: %d: ", ledArray[i].greenVal);
+            printf("\nError: greenVal not initialized correctly. greenVal: %d: ", ledArray[i].greenVal);
             return 1;
         }
         if ( ledArray[i].blueVal != 255 ){
-            printf("Error: blueVal not initialized correctly. blueVal: %d: ", ledArray[i].blueVal);
+            printf("\nError: blueVal not initialized correctly. blueVal: %d: ", ledArray[i].blueVal);
             return 1;
         }
         if ( ledArray[i].brightness != 100 ){
-            printf("Error: brightness not initialized correctly. brightness: %d: ", ledArray[i].brightness);
+            printf("\nError: brightness not initialized correctly. brightness: %d: ", ledArray[i].brightness);
             return 1;
         }
     }
@@ -106,14 +108,15 @@ int main(void){
     // Change a single LED's brightness in a valid way
     int result = change_LED_Brightness(&ledArray[0], 30);
     if ( ledArray[0].brightness != 30 ){
-        printf("Error changing a single LED's brightness in a valid way");
+        printf("\nError changing a single LED's brightness in a valid way");
+        printf("\nLED in question's brightness: %d", ledArray[0].brightness);
         return 2;
     }
 
     // Try changing a single LED's brightness in an invalid way
     result = change_LED_Brightness(&ledArray[0], -1);
     if ( ( result == 0 ) || (ledArray[0].brightness == -1 ) ){
-        printf("Error changing a single LED's brightness in an invalid way");
+        printf("\nError changing a single LED's brightness in an invalid way");
         return 2;
     }
 
@@ -121,14 +124,14 @@ int main(void){
     for ( int i=0; i<length; i++){
         result = change_LED_Brightness(&ledArray[i], i % 255);
         if ( ( result != 0 ) || ( ledArray[i].brightness != (i % 255) ) ){
-            printf("Error changing all LED's brightnesses in a valid way during assignment");
+            printf("\nError changing all LED's brightnesses in a valid way during assignment");
             return 2;
         }
     }
     // Make sure none of the LED's brightnesses affected each other
     for ( int i=0; i<length; i++){
         if ( ledArray[i].brightness != (i % 255) ){
-            printf("Error changing all LED's brightnesses in a valid way after assignment");
+            printf("\nError changing all LED's brightnesses in a valid way after assignment");
             return 2;
         }
     }
@@ -138,14 +141,14 @@ int main(void){
     // Change a single LED's color in a valid way
     result = change_LED_Color(&ledArray[0], 30, 60, 90);
     if ( ( ledArray[0].redVal != 30 ) || ( ledArray[0].greenVal != 60 ) || ( ledArray[0].blueVal != 90 ) ){
-        printf("Error changing a single LED's color in a valid way");
+        printf("\nError changing a single LED's color in a valid way");
         return 2;
     }
 
     // Change a single LED's color in an invalid way
     result = change_LED_Color(&ledArray[0], 256, -1, 100000);
     if ( ( ledArray[0].redVal == 256 ) || ( ledArray[0].greenVal == -1 ) || ( ledArray[0].blueVal == 100000 ) || ( result == 0 ) ){
-        printf("Error changing a single LED's color in an invalid way");
+        printf("\nError changing a single LED's color in an invalid way");
         return 2;
     }
 
