3e4e9322c4a09ae510bc28596fb27628bb11a588
Peter Walsh <peterbug80228@gmail.com>
2022-11-22 23:48:08 -0700

    Save work from before realization that the specification says LEDs can only be in 4 color states

--- README.txt
+++ README.txt
@@ -1,3 +1,10 @@
+Unfortunately, I ran out of time to complete this task. As a result, Iontra_RGB_LED_INTF.c is not a good representation of how a microcontroller would control LEDs. 
+Additionally, STM32_RGB_LED_INTF.c is entirely comments describing what I would've liked to have written if I had had the time to go through and understand the documentation.
+
+I want to make it clear I do understand that the point was to make the STM32 implementation very similar to the general version and that mine is not because it is all comments.
+I'm hoping that the commit history makes it clear that I really did try to make it work but at this point I am out of time so I wanted to submit something that demonstrates
+both that I know the theory behind driving LEDs with microcontrollers via PWM and that I can make a general-ish interface that could be adaptable for specific implementations.
+
 About each file:
  - LED.h is a basic header file which declares the 3 functions which are required in the interfaces.
  
@@ -5,8 +12,6 @@ About each file:
    It contains the very basic idea of what an LED interface might look like but would need modification to actually work.
  
  - STM32_RGB_LED_INTF.c is an example of what it would look like to modify Iontra_RGB_LED_INTF.c to work with an STM32 microcontroller.
-   It most likely wouldn't actually work in its current state, as it hasn't been tested but it demonstrates the things that would need to be added to Iontra_RGB_LED_INTF.c to make it functional.
-   All STM32 related things are commented out so the code will compile. All lines to be theoretically uncommented have a ~ immediately after the //.
    I used this manual for reference:  https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj44f_hvMP7AhVTKH0KHclvCHEQFnoECA0QAQ&url=https%3A%2F%2Fwww.st.com%2Fresource%2Fen%2Fuser_manual%2Fum1725-description-of-stm32f4-hal-and-lowlayer-drivers-stmicroelectronics.pdf&usg=AOvVaw21r2dLlr83WM6rfjwZ3NM-
    I also used this tutorial for reference: https://controllerstech.blogspot.com/2019/02/interface-rgb-led-with-stm32.html
 
--- STM32_RGB_LED_INTF.c
+++ STM32_RGB_LED_INTF.c
@@ -1,48 +1,30 @@
 #include <stdio.h>
 #include "LED.h"
 
-//~#include "stm32f4xx_hal.h"
-//~#include "stm32f4xx_hal_tim.h"
-
 // Initialize the LED objects
 // Return 0: LEDs successfully initialized
 // Return 1: One or more of the input values was out of bounds
-int init_LEDs(Led* ledArray, int length){
-    // length cannot be less than 1 because that would result in errors further on
-    if ( length < 1 ){
-        return 1;
-    }
-
-    // Initialize timer module for PWM usage
-    //~ TIM_Base_InitTypeDef base = {}
-    //~ TIM_HandleTypeDef htim; 
-    //~ HAL_TIM_PWM_MspInit(&htim);
-    //~ __HAL_RCC_TIMx_CLK_ENABLE(&htim);
-    //~ HAL_TIM_PWM_Init(&htim);
-    //~ HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_1);
-    //~ HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_2);
-    //~ HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_3);
-
+int init_LEDs(Timer* pwmTimerArray, int length){
     // Initialize LEDs to bright white - easiest to see if there is a problem
-    for ( int i=0; i<length; i++ ){
-        ledArray[i].redVal = 255;
-        ledArray[i].greenVal = 255;
-        ledArray[i].blueVal = 255;
-        ledArray[i].brightness = 100;
-    }
-    
+
+    // For each RGB LED:
+        // Initialize PWM clock on microprocessor - 3 channels
+        // Set duty cycle to 100% for all 3 channels used
+
     return 0;
 }
 
 // Change the brightness of the given LEDobject to a value given by targetBrightness (between 0-100)
 // Return 0: Brightness successfully changed
 // Return 1: One or more of the input values was out of bounds 
-int change_LED_Brightness(Led* led_pointer, int newBrightness){
-    if ( (newBrightness < 0) || (newBrightness > 100) ){
-        return 1;
-    }
+int change_LED_Brightness(Timer* pwmTimer, int newBrightness){
     
-    led_pointer->brightness = newBrightness;
+    // Consider the current colors and brightness.
+    // For example, one could have bright white at 50% brightness which would lead to the duty cycle for all 3 channels to be 50%
+    // One could also have a solid red at 50% brightness which would lead to the duty cycle for the red channel to be 50% and the other 2 to be 0%
+    // Therefore, adjusting the duty cycle unilaterally would not work, as the colors would not be preserved. 
+    // Instead, the ratio between the 3 channels should be preserved and the highest value should be scaled to the new brightness.
+    // For example, if one had (30, 150, 60) and this function was called with a newBrightness of 10%
     
     return 0;
 }
@@ -52,15 +34,8 @@ int change_LED_Brightness(Led* led_pointer, int newBrightness){
 // Return 1: One or more of the input values was out of bounds
 // *RGB values could be uint8, as they range from 0-255* - not done here because I don't think uint8 is native to C
 int change_LED_Color(Led* led_pointer, int newRedVal, int newGreenVal, int newBlueVal){
-    if ( ( (newRedVal > 255) || (newRedVal < 0) ) || ( (newGreenVal > 255) || (newGreenVal < 0) ) || ( (newBlueVal > 255) || (newBlueVal < 0) ) ){
-        return 1;
-    }
-
-    led_pointer->redVal = newRedVal;
-    led_pointer->greenVal = newGreenVal;
-    led_pointer->blueVal = newBlueVal;
-
-    return 0;
+    
+    // Change the 
 }
 
 // Prints out all the values in the ledArray
@@ -80,15 +55,11 @@ void printLEDs(Led* ledArray, int length){
 // Return 3 if there is a problem with changing colors
 // *NOTE* TESTS SHOWN HERE ARE NOT EXHAUSTIVE
 int main(void){
-    // HAL initialization
-    
-
-
     // Create an empty array that can hold up to 5 Led structs
     Led ledArray[5];
 
     // Use a variable to hold length for easy modification
-    int length = 1;
+    int length = 5;
     init_LEDs(ledArray, length);
     
     // Test that the LEDs inside ledArray got initialized like I think
