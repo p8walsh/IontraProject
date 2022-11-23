f2e9821242db0b48477d098e06bf8366257c3ba7
Peter Walsh <peterbug80228@gmail.com>
2022-11-22 23:13:33 -0700

    Beginnings of implementing STM32 HAL parts

--- README.txt
+++ README.txt
@@ -1,4 +1,14 @@
-Goal is to have LED implementation for RGB LED on STM32 very similar to generic LED interface. SIMILARITY IS IMPORTANT for simple transition between microcontrollers.
+About each file:
+ - LED.h is a basic header file which declares the 3 functions which are required in the interfaces.
+ 
+ - Iontra_RGB_LED_INTF.c is the more generic interface file that doesn't contain anything specific to any specific microcontroller.
+   It contains the very basic idea of what an LED interface might look like but would need modification to actually work.
+ 
+ - STM32_RGB_LED_INTF.c is an example of what it would look like to modify Iontra_RGB_LED_INTF.c to work with an STM32 microcontroller.
+   It most likely wouldn't actually work in its current state, as it hasn't been tested but it demonstrates the things that would need to be added to Iontra_RGB_LED_INTF.c to make it functional.
+   All STM32 related things are commented out so the code will compile. All lines to be theoretically uncommented have a ~ immediately after the //.
+   I used this manual for reference:  https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwj44f_hvMP7AhVTKH0KHclvCHEQFnoECA0QAQ&url=https%3A%2F%2Fwww.st.com%2Fresource%2Fen%2Fuser_manual%2Fum1725-description-of-stm32f4-hal-and-lowlayer-drivers-stmicroelectronics.pdf&usg=AOvVaw21r2dLlr83WM6rfjwZ3NM-
+   I also used this tutorial for reference: https://controllerstech.blogspot.com/2019/02/interface-rgb-led-with-stm32.html
 
 NOTE: Tests shown in Iontra_RGB_LED_INTF.c and STM32_RGB_LED_INTF.c are NOT EXHAUSTIVE/THOROUGH. I created them mostly to test myself, but if I had more time
 I would've created more tests to try to exhaustively check to ensure everything works properly.
--- STM32_RGB_LED_INTF.c
+++ STM32_RGB_LED_INTF.c
@@ -1,5 +1,166 @@
 #include <stdio.h>
-int main() {
-   printf("Hello, World!");
-   return 0;
+#include "LED.h"
+
+//~#include "stm32f4xx_hal.h"
+//~#include "stm32f4xx_hal_tim.h"
+
+// Initialize the LED objects
+// Return 0: LEDs successfully initialized
+// Return 1: One or more of the input values was out of bounds
+int init_LEDs(Led* ledArray, int length){
+    // length cannot be less than 1 because that would result in errors further on
+    if ( length < 1 ){
+        return 1;
+    }
+
+    // Initialize timer module for PWM usage
+    //~ TIM_Base_InitTypeDef base = {}
+    //~ TIM_HandleTypeDef htim; 
+    //~ HAL_TIM_PWM_MspInit(&htim);
+    //~ __HAL_RCC_TIMx_CLK_ENABLE(&htim);
+    //~ HAL_TIM_PWM_Init(&htim);
+    //~ HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_1);
+    //~ HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_2);
+    //~ HAL_TIM_PWM_Start(&htim, TIM_CHANNEL_3);
+
+    // Initialize LEDs to bright white - easiest to see if there is a problem
+    for ( int i=0; i<length; i++ ){
+        ledArray[i].redVal = 255;
+        ledArray[i].greenVal = 255;
+        ledArray[i].blueVal = 255;
+        ledArray[i].brightness = 100;
+    }
+    
+    return 0;
+}
+
+// Change the brightness of the given LEDobject to a value given by targetBrightness (between 0-100)
+// Return 0: Brightness successfully changed
+// Return 1: One or more of the input values was out of bounds 
+int change_LED_Brightness(Led* led_pointer, int newBrightness){
+    if ( (newBrightness < 0) || (newBrightness > 100) ){
+        return 1;
+    }
+    
+    led_pointer->brightness = newBrightness;
+    
+    return 0;
+}
+
+// Change the color of the given LEDobject LED based on the RGB values given.
+// Return 0: Colors successfully changed
+// Return 1: One or more of the input values was out of bounds
+// *RGB values could be uint8, as they range from 0-255* - not done here because I don't think uint8 is native to C
+int change_LED_Color(Led* led_pointer, int newRedVal, int newGreenVal, int newBlueVal){
+    if ( ( (newRedVal > 255) || (newRedVal < 0) ) || ( (newGreenVal > 255) || (newGreenVal < 0) ) || ( (newBlueVal > 255) || (newBlueVal < 0) ) ){
+        return 1;
+    }
+
+    led_pointer->redVal = newRedVal;
+    led_pointer->greenVal = newGreenVal;
+    led_pointer->blueVal = newBlueVal;
+
+    return 0;
+}
+
+// Prints out all the values in the ledArray
+void printLEDs(Led* ledArray, int length){
+    for ( int i=0; i<length; i++ ){
+        printf("LED %d: ", i);
+        printf("redVal: %d, ", ledArray[i].redVal);
+        printf("greenVal: %d, ", ledArray[i].greenVal);
+        printf("blueVal: %d, ", ledArray[i].blueVal);
+        printf("brightness: %d.\n", ledArray[i].brightness);
+    }
+}
+
+// Return 0 if everything works correctly
+// Return 1 if there is an initialization error
+// Return 2 if there is a problem with changing brightness
+// Return 3 if there is a problem with changing colors
+// *NOTE* TESTS SHOWN HERE ARE NOT EXHAUSTIVE
+int main(void){
+    // HAL initialization
+    
+
+
+    // Create an empty array that can hold up to 5 Led structs
+    Led ledArray[5];
+
+    // Use a variable to hold length for easy modification
+    int length = 1;
+    init_LEDs(ledArray, length);
+    
+    // Test that the LEDs inside ledArray got initialized like I think
+    printLEDs(ledArray, length);
+    
+    // Use code to test what is being visually confirmed above
+    for ( int i=0; i<length; i++ ){
+        if ( ledArray[i].redVal != 255 ){
+            printf("Error: redVal not initialized correctly. redVal: %d: \n", ledArray[i].redVal);
+            return 1;
+        }
+        if ( ledArray[i].greenVal != 255 ){
+            printf("Error: greenVal not initialized correctly. greenVal: %d: \n", ledArray[i].greenVal);
+            return 1;
+        }
+        if ( ledArray[i].blueVal != 255 ){
+            printf("Error: blueVal not initialized correctly. blueVal: %d: \n", ledArray[i].blueVal);
+            return 1;
+        }
+        if ( ledArray[i].brightness != 100 ){
+            printf("Error: brightness not initialized correctly. brightness: %d: \n", ledArray[i].brightness);
+            return 1;
+        }
+    }
+
+    // Test changing brightness
+    
+    // Change a single LED's brightness in a valid way
+    int result = change_LED_Brightness(&ledArray[0], 30);
+    if ( ledArray[0].brightness != 30 ){
+        printf("Error changing a single LED's brightness in a valid way\n");
+        return 2;
+    }
+
+    // Try changing a single LED's brightness in an invalid way
+    result = change_LED_Brightness(&ledArray[0], -1);
+    if ( ( result == 0 ) || (ledArray[0].brightness == -1 ) ){
+        printf("Error changing a single LED's brightness in an invalid way\n");
+        return 2;
+    }
+
+    // Try changing all the LED's brightnesses in a valid way
+    for ( int i=0; i<length; i++){
+        result = change_LED_Brightness(&ledArray[i], i % 255);
+        if ( ( result != 0 ) || ( ledArray[i].brightness != (i % 255) ) ){
+            printf("Error changing all LED's brightnesses in a valid way during assignment\n");
+            return 2;
+        }
+    }
+    // Make sure none of the LED's brightnesses affected each other
+    for ( int i=0; i<length; i++){
+        if ( ledArray[i].brightness != (i % 255) ){
+            printf("Error changing all LED's brightnesses in a valid way after assignment\n");
+            return 2;
+        }
+    }
+
+    // Test changing color
+
+    // Change a single LED's color in a valid way
+    result = change_LED_Color(&ledArray[0], 30, 60, 90);
+    if ( ( ledArray[0].redVal != 30 ) || ( ledArray[0].greenVal != 60 ) || ( ledArray[0].blueVal != 90 ) ){
+        printf("Error changing a single LED's color in a valid way\n");
+        return 2;
+    }
+
+    // Change a single LED's color in an invalid way
+    result = change_LED_Color(&ledArray[0], 256, -1, 100000);
+    if ( ( ledArray[0].redVal == 256 ) || ( ledArray[0].greenVal == -1 ) || ( ledArray[0].blueVal == 100000 ) || ( result == 0 ) ){
+        printf("Error changing a single LED's color in an invalid way\n");
+        return 2;
+    }
+
+    return 0;
 }
